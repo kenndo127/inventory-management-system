0f4db1f7b0ded890d4d289738616a27a715ba2e8
Kenneth Okechukwu <kennethchidiebube5@gmail.com>
2026-03-23 22:07:31 +0100

    finished the menu and add product method.

--- src/main/java/org/kenneth/inventoryManagement/main/Main.java
+++ src/main/java/org/kenneth/inventoryManagement/main/Main.java
@@ -1,43 +1,45 @@
 package org.kenneth.inventoryManagement.main;
 
+import java.util.Scanner;
+
 public class Main {
     // create a static variable here
+    static Scanner scan = new Scanner(System.in);
+    static InventoryRepository inventoryRepository = new InventoryRepository();
+    static InventoryService inventoryService = new InventoryService(inventoryRepository);
+
     public static void main(String[] args) {
 
 
-        //Creating product objects
-        Product product1 = new Product("1", "phone", 334.0, 5, "tech");
-        Product product2 = new Product("2", "laptop", 334.0, 10, "tech");
+        System.out.println("Welcome to my Inventory Management App");
+        System.out.println("""
+                1. Add a new product
+                2. Update the previous product quantity
+                3. Search a product
+                4. List Products
+                5. See total inventory value
+                6. Remove an existing product
+                """);
+        System.out.print("Menu (Enter a choice): ");
+        int menuChoice = scan.nextInt();
 
-        //Creating InventoryService Objects
-        InventoryRepository inventoryRepository = new InventoryRepository();
-        InventoryService inventoryService = new InventoryService(inventoryRepository);
+        System.out.println(menuChoice);
+    }
+    private static void addProduct(){
+        System.out.println("Fill in the values");
 
-        //adding products
-        inventoryService.addProduct(product1);
-        inventoryService.addProduct(product2);
+        System.out.println("Product_id: ");
+        String id = scan.next();
+        System.out.println("Product_name: ");
+        String productName = scan.next();
+        System.out.println("Product_price: ");
+        double price = scan.nextDouble();
+        System.out.println("Product_quantity: ");
+        int quantity = scan.nextInt();
+        System.out.println("Product_category: ");
+        String category = scan.next();
 
-        //testing the update quantity
-        System.out.println("before 1:" + product1.getQuantity());
-        System.out.println("before 2:" + product2.getQuantity());
-        inventoryService.updateQuantity("phone", 10);
-        inventoryService.updateQuantity("laptop", 20);
-        System.out.println("after 1:" + product1.getQuantity());
-        System.out.println("after 2:" + product2.getQuantity());
-
-        //testing the search feature
-        System.out.println("\n" + inventoryService.search("LaptoP"));
-
-        //testing the list by sorted price
-        inventoryService.listSortedByPrice();
-
-        //testing total inventory value
-        System.out.println(inventoryService.totalInventoryValue());
-
-        //testing remove product
-        inventoryService.removeProduct("phone");
-        //searching removed object to be sure it doesn't exist anymore
-        System.out.println("\n" + inventoryService.search("phone"));
-
+        inventoryService.addProduct(new Product(id, productName, price, quantity, category));
+        System.out.println(productName + "has been added to the Inventory");
     }
 }
