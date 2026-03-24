2e8c4750f389a838d49bab9267ec6be21d90cddf
Kenneth Okechukwu <kennethchidiebube5@gmail.com>
2026-03-24 13:39:49 +0100

    Removed setters for product fields. Final touches.

--- src/main/java/org/kenneth/inventoryManagement/entity/Product.java
+++ src/main/java/org/kenneth/inventoryManagement/entity/Product.java
@@ -19,24 +19,12 @@ public class Product {
         return id;
     }
 
-    public void setId(String id) {
-        this.id = id;
-    }
-
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public double getPrice() {
         return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
     }
 
     public int getQuantity() {
@@ -49,10 +37,6 @@ public class Product {
 
     public String getCategory() {
         return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
     }
 
     @Override
